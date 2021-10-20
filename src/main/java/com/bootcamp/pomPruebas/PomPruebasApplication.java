b8b02f804a87ddfc814cec146d4e32a86fa8528c
Francisco Tomas Guzman Fernandez <francistomas96@gmail.com>
2021-10-20 15:13:29 +0200

    llamamos al addusuario() de autoconfigure para mostrar el usuario

--- src/main/java/com/bootcamp/pomPruebas/PomPruebasApplication.java
+++ src/main/java/com/bootcamp/pomPruebas/PomPruebasApplication.java
@@ -1,12 +1,8 @@
 package com.bootcamp.pomPruebas;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.bootcamp.pomPruebas.controller.IndexController;
-import com.nttdata.bootcamp.saludoIdioma.config.SaludoProperties;
-import com.nttdata.bootcamp.saludoIdioma.repository.Saludo;
 
 @SpringBootApplication
 public class PomPruebasApplication {
